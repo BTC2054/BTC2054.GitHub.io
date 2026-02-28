d098a828e0c5165e3eb5e64b72ce9bbbf748af99
2054 <90368994+BTC2054@users.noreply.github.com>
2026-02-28 21:04:07 +0800

    Add files via upload

---
+++ blog_data.js
@@ -0,0 +1,323 @@
+/* 数据库文件 */
+window.blogConfig = {
+    "title": "BTC 2054",
+    "subtitle": "Salvation lies within",
+    "avatar": "https://www.btc2054.com/IMG/90368994.jpg",
+    "browserTitle": "欢迎来到 2054 的博客",
+    "favicon": "https://www.btc2054.com/IMG/90368994.jpg"
+};
+window.menuData = [
+    {
+        "id": "m1",
+        "code": "1",
+        "name": "首页",
+        "children": []
+    },
+    {
+        "id": "m2",
+        "code": "2",
+        "name": "我的计划",
+        "children": [
+            {
+                "id": "m1735640348731",
+                "code": "2-1",
+                "name": "2054养老计划",
+                "children": []
+            },
+            {
+                "id": "m1768918729047",
+                "code": "2-2",
+                "name": "100单计划",
+                "children": []
+            }
+        ]
+    },
+    {
+        "id": "m1767531461774",
+        "code": "3",
+        "name": "技术分享",
+        "children": []
+    }
+];
+window.postData = [
+    {
+        "title": "2026年01月：定存第22/360期、+4%",
+        "date": "2026-01-29",
+        "categoryId": "m1735640348731",
+        "url": "https://www.btc2054.com/Blog/2054YangLao/202601/202601.html",
+        "globalTop": false,
+        "categoryTop": false,
+        "globalTopTime": null,
+        "categoryTopTime": null
+    },
+    {
+        "title": "飞利浦S2000剃须刀自换电池维修记录",
+        "date": "2026-01-20",
+        "categoryId": "m1767531461774",
+        "url": "https://www.btc2054.com/Blog/JishuFenxiang/%E9%A3%9E%E5%88%A9%E6%B5%A6S2000/1.html",
+        "globalTop": false,
+        "categoryTop": false,
+        "globalTopTime": null,
+        "categoryTopTime": null
+    },
+    {
+        "title": "Notion自定义分割线颜色、长短、粗细",
+        "date": "2026-01-04",
+        "categoryId": "m1767531461774",
+        "url": "https://www.btc2054.com/Blog/JishuFenxiang/Notion%E8%87%AA%E5%AE%9A%E4%B9%89%E5%88%86%E5%89%B2%E7%BA%BF/22a46f9b8afe80bab60be71911afd6fa.html",
+        "globalTop": false,
+        "categoryTop": false,
+        "globalTopTime": null,
+        "categoryTopTime": null
+    },
+    {
+        "title": "个人博客的终极形态？",
+        "date": "2026-01-04",
+        "categoryId": "m1767531461774",
+        "url": "https://www.btc2054.com/Blog/JishuFenxiang/%E4%B8%AA%E4%BA%BA%E5%8D%9A%E5%AE%A2%E7%9A%84%E7%BB%88%E6%9E%81%E5%BD%A2%E6%80%81/23746f9b8afe80289ea5c008e1ddfa5d.html",
+        "globalTop": false,
+        "categoryTop": false,
+        "globalTopTime": null,
+        "categoryTopTime": null
+    },
+    {
+        "title": "Notion文本字体居中",
+        "date": "2026-01-04",
+        "categoryId": "m1767531461774",
+        "url": "https://www.btc2054.com/Blog/JishuFenxiang/Notion%E6%96%87%E6%9C%AC%E5%AD%97%E4%BD%93%E5%B1%85%E4%B8%AD/22746f9b8afe8011b2a8da44520ac9e1.html",
+        "globalTop": false,
+        "categoryTop": false,
+        "globalTopTime": null,
+        "categoryTopTime": null
+    },
+    {
+        "title": "2025年12月：定存第21/360期、+8.5%、首个里程碑",
+        "date": "2025-12-31",
+        "categoryId": "m1735640348731",
+        "url": "https://www.btc2054.com/Blog/2054YangLao/202512/202512.html",
+        "globalTop": false,
+        "categoryTop": false,
+        "globalTopTime": null,
+        "categoryTopTime": null
+    },
+    {
+        "title": "2025年11月：定存第20/360期、-0.41%",
+        "date": "2025-11-30",
+        "categoryId": "m1735640348731",
+        "url": "https://www.btc2054.com/Blog/2054YangLao/202511/202511.html",
+        "globalTop": false,
+        "categoryTop": false,
+        "globalTopTime": null,
+        "categoryTopTime": null
+    },
+    {
+        "title": "No.20250424策略《100单》实盘测试计划",
+        "date": "2025-11-28",
+        "categoryId": "m1768918729047",
+        "url": "https://www.btc2054.com/Blog/Plan/100/1f646f9b8afe80fbb31bffe25bb77236.html",
+        "globalTop": false,
+        "categoryTop": false,
+        "globalTopTime": null,
+        "categoryTopTime": null
+    },
+    {
+        "title": "2025年10月：定存第19/360期、+0.87%、更新计划V1.1",
+        "date": "2025-10-31",
+        "categoryId": "m1735640348731",
+        "url": "https://www.btc2054.com/Blog/2054YangLao/202510/202510.html",
+        "globalTop": false,
+        "categoryTop": false,
+        "globalTopTime": null,
+        "categoryTopTime": null
+    },
+    {
+        "title": "2025年09月：定存第18/360期、+2.58%",
+        "date": "2025-09-30",
+        "categoryId": "m1735640348731",
+        "url": "https://www.btc2054.com/Blog/2054YangLao/202509/202509.html",
+        "globalTop": false,
+        "categoryTop": false,
+        "globalTopTime": null,
+        "categoryTopTime": null
+    },
+    {
+        "title": "2025年08月：定存第17/360期、+1.66%",
+        "date": "2025-08-31",
+        "categoryId": "m1735640348731",
+        "url": "https://www.btc2054.com/Blog/2054YangLao/202508/202508.html",
+        "globalTop": false,
+        "categoryTop": false,
+        "globalTopTime": null,
+        "categoryTopTime": null
+    },
+    {
+        "title": "2025年07月：定存第16/360期、+10.48%",
+        "date": "2025-07-31",
+        "categoryId": "m1735640348731",
+        "url": "https://www.btc2054.com/Blog/2054YangLao/202507/202507.html",
+        "globalTop": false,
+        "categoryTop": false,
+        "globalTopTime": null,
+        "categoryTopTime": null
+    },
+    {
+        "title": "2025年06月：定存第15/360期、+1.56%",
+        "date": "2025-06-30",
+        "categoryId": "m1735640348731",
+        "url": "https://www.btc2054.com/Blog/2054YangLao/202506/202506.html",
+        "globalTop": false,
+        "categoryTop": false,
+        "globalTopTime": null,
+        "categoryTopTime": null
+    },
+    {
+        "title": "2025年05月：定存第14/360期、+20.7%",
+        "date": "2025-05-31",
+        "categoryId": "m1735640348731",
+        "url": "https://www.btc2054.com/Blog/2054YangLao/202505/202505.html",
+        "globalTop": false,
+        "categoryTop": false,
+        "globalTopTime": null,
+        "categoryTopTime": null
+    },
+    {
+        "title": "2025年04月：定存第13/360期、-0.56%",
+        "date": "2025-04-30",
+        "categoryId": "m1735640348731",
+        "url": "https://www.btc2054.com/Blog/2054YangLao/202504/202504.html",
+        "globalTop": false,
+        "categoryTop": false,
+        "globalTopTime": null,
+        "categoryTopTime": null
+    },
+    {
+        "title": "2025年03月：定存第12/360期、+0.26%",
+        "date": "2025-03-31",
+        "categoryId": "m1735640348731",
+        "url": "https://www.btc2054.com/Blog/2054YangLao/202503/202503.html",
+        "globalTop": false,
+        "categoryTop": false,
+        "globalTopTime": null,
+        "categoryTopTime": null
+    },
+    {
+        "title": "2025年02月：定存第11/360期、-7.37%",
+        "date": "2025-02-28",
+        "categoryId": "m1735640348731",
+        "url": "https://www.btc2054.com/Blog/2054YangLao/202502/202502.html",
+        "globalTop": false,
+        "categoryTop": false,
+        "globalTopTime": null,
+        "categoryTopTime": null
+    },
+    {
+        "title": "2025年01月：定存第10/360期、+1%",
+        "date": "2025-01-31",
+        "categoryId": "m1735640348731",
+        "url": "https://www.btc2054.com/Blog/2054YangLao/202501/202501.html",
+        "globalTop": false,
+        "categoryTop": false,
+        "globalTopTime": null,
+        "categoryTopTime": null
+    },
+    {
+        "title": "2024年12月：定存第9/360期、回归定投本质？？",
+        "date": "2024-12-31",
+        "categoryId": "m1735640348731",
+        "url": "https://www.btc2054.com/Blog/2054YangLao/202412/202412.html",
+        "globalTop": false,
+        "categoryTop": false,
+        "globalTopTime": null,
+        "categoryTopTime": null
+    },
+    {
+        "title": "2024年11月：定存第8/360期、终于解套了!!!",
+        "date": "2024-11-30",
+        "categoryId": "m1735640348731",
+        "url": "https://www.btc2054.com/Blog/2054YangLao/202411/202411.html",
+        "globalTop": false,
+        "categoryTop": false,
+        "globalTopTime": null,
+        "categoryTopTime": null
+    },
+    {
+        "title": "2024年09-10月：定存第6-7/360期、持续被套中。。。",
+        "date": "2024-10-31",
+        "categoryId": "m1735640348731",
+        "url": "https://www.btc2054.com/Blog/2054YangLao/202409-10/202409-10.html",
+        "globalTop": false,
+        "categoryTop": false,
+        "globalTopTime": null,
+        "categoryTopTime": null
+    },
+    {
+        "title": "2024年08月：定存第5/360期、被套中",
+        "date": "2024-08-31",
+        "categoryId": "m1735640348731",
+        "url": "https://www.btc2054.com/Blog/2054YangLao/202408/202408.html",
+        "globalTop": false,
+        "categoryTop": false,
+        "globalTopTime": null,
+        "categoryTopTime": null
+    },
+    {
+        "title": "2024年07月：定存第4/360期、+5.18%",
+        "date": "2024-07-31",
+        "categoryId": "m1735640348731",
+        "url": "https://www.btc2054.com/Blog/2054YangLao/202407/202407.html",
+        "globalTop": false,
+        "categoryTop": false,
+        "globalTopTime": null,
+        "categoryTopTime": null
+    },
+    {
+        "title": "2024年06月：定存第3/360期、+2.97%",
+        "date": "2024-06-30",
+        "categoryId": "m1735640348731",
+        "url": "https://www.btc2054.com/Blog/2054YangLao/202406/202406.html",
+        "globalTop": false,
+        "categoryTop": false,
+        "globalTopTime": null,
+        "categoryTopTime": null
+    },
+    {
+        "title": "2024年05月：定存第2/360期、+1.10%",
+        "date": "2024-05-31",
+        "categoryId": "m1735640348731",
+        "url": "https://www.btc2054.com/Blog/2054YangLao/202405/202405.html",
+        "globalTop": false,
+        "categoryTop": false,
+        "globalTopTime": null,
+        "categoryTopTime": null
+    },
+    {
+        "title": "2024年04月：定存第1/360期、+7.04% ",
+        "date": "2024-04-30",
+        "categoryId": "m1735640348731",
+        "url": "https://www.btc2054.com/Blog/2054YangLao/202404/202404.html",
+        "globalTop": false,
+        "categoryTop": false,
+        "globalTopTime": null,
+        "categoryTopTime": null
+    },
+    {
+        "title": "2024年4月：定存初始篇",
+        "url": "https://www.btc2054.com/Blog/2054YangLao/202404chushi/202604chushi.html",
+        "date": "2024-04-01",
+        "categoryId": "m1735640348731",
+        "globalTop": false,
+        "categoryTop": true,
+        "globalTopTime": null,
+        "categoryTopTime": 1769864551501
+    },
+    {
+        "title": "2026年02月：定存第23/360期、-23.6%、现货被套",
+        "url": "https://btc2054.com/Blog/2054YangLao/202602/202602.html",
+        "date": "2026-02-28",
+        "categoryId": "m1735640348731",
+        "globalTop": false,
+        "categoryTop": false,
+        "globalTopTime": null,
+        "categoryTopTime": null
+    }
+];
